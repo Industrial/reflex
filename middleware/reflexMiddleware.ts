4f70754c9446304a1cf24b026c7a4cf078ac25dd
Tom Wieland <tom.wieland@gmail.com>
2022-06-02 22:00:41 +0200

    fix: forgot to pass Document to reflexMiddleware

--- middleware/reflexMiddleware.ts
+++ middleware/reflexMiddleware.ts
@@ -1,31 +1,38 @@
+import { ElementType } from 'https://esm.sh/react@18.1.0';
 import { Middleware } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
-import { resolveLocalPath } from '../path.ts';
 
 import { appSourceMiddleware } from './appSourceMiddleware.ts';
+import { resolveLocalPath } from '../path.ts';
 import { serverSideRenderMiddleware } from './serverSideRenderMiddleware.tsx';
 import { staticFileMiddleware } from './staticFileMiddleware.ts';
 import { vendorSourceMiddleware } from './vendorSourceMiddleware.ts';
 
 export type ReflexMiddlewareProps = {
-  appSourcePrefix: string;
-  vendorSourcePrefix: string;
+  Document: ElementType;
+  appSourcePrefix?: string;
   importMapPath?: string;
+  vendorSourcePrefix?: string;
 };
 
 export const reflexMiddleware = async ({
-  appSourcePrefix,
-  vendorSourcePrefix,
+  Document,
+  appSourcePrefix = '/.x',
   importMapPath = resolveLocalPath('./importMap.json'),
+  vendorSourcePrefix = '/.v',
 }: ReflexMiddlewareProps) => {
   const appSource = await appSourceMiddleware({
     appSourcePrefix,
     vendorSourcePrefix,
     importMapPath,
   });
+
   const serverSideRender = serverSideRenderMiddleware({
+    Document,
     vendorSourcePrefix,
   });
+
   const staticFile = staticFileMiddleware();
+
   const vendorSource = await vendorSourceMiddleware({
     appSourcePrefix,
     vendorSourcePrefix,
--- middleware/serverSideRenderMiddleware.tsx
+++ middleware/serverSideRenderMiddleware.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { ElementType } from 'https://esm.sh/react@18.1.0';
 import { renderToReadableStream } from 'https://esm.sh/react-dom@18.1.0/server?dev';
 import { Middleware } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 
-import { Document } from '../../app/Document.tsx';
-
 export type ServerSideRenderMiddlewareProps = {
+  Document: ElementType;
   vendorSourcePrefix: string;
 };
 
-export const serverSideRenderMiddleware = (
-  { vendorSourcePrefix }: ServerSideRenderMiddlewareProps,
-) => {
+export const serverSideRenderMiddleware = ({
+  Document,
+  vendorSourcePrefix,
+}: ServerSideRenderMiddlewareProps) => {
   const middleware: Middleware = async (ctx) => {
     ctx.response.headers.set('Content-Type', 'text/html; charset=utf-8');
 
