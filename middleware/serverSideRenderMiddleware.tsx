61f6abc037d8a92793f689ec2fc3d589eb964f59
Tom Wieland <tom.wieland@gmail.com>
2022-06-05 20:54:21 +0200

    feature(ssr): pass the request to Document

--- middleware/serverSideRenderMiddleware.tsx
+++ middleware/serverSideRenderMiddleware.tsx
@@ -1,9 +1,16 @@
-import React, { ElementType } from 'https://esm.sh/react@18.1.0';
+import React from 'https://esm.sh/react@18.1.0';
 import { renderToReadableStream } from 'https://esm.sh/react-dom@18.1.0/server?dev';
-import { Middleware } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
+import { Middleware, Request } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
+
+export type DocumentProps = {
+  request: Request;
+  vendorSourcePrefix: string;
+};
+
+export type DocumentComponent = (props: DocumentProps) => JSX.Element;
 
 export type ServerSideRenderMiddlewareProps = {
-  Document: ElementType;
+  Document: DocumentComponent;
   modifyStream?: (applicationStream: ReadableStream) => ReadableStream;
   vendorSourcePrefix: string;
 };
@@ -17,7 +24,10 @@ export const serverSideRenderMiddleware = ({
     ctx.response.headers.set('Content-Type', 'text/html; charset=utf-8');
 
     let stream: ReadableStream = await renderToReadableStream(
-      <Document vendorSourcePrefix={vendorSourcePrefix} />,
+      <Document
+        request={ctx.request}
+        vendorSourcePrefix={vendorSourcePrefix}
+      />,
     );
 
     if (modifyStream) {
