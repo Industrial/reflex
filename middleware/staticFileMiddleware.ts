3093a0ebd2374594b556bc0e90264f0a8e0baccd
Tom Wieland <tom.wieland@gmail.com>
2022-06-05 18:54:21 +0200

    fix(middleware): when static files aren't found just continue

--- middleware/staticFileMiddleware.ts
+++ middleware/staticFileMiddleware.ts
@@ -2,13 +2,17 @@ import { Middleware } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 
 export const staticFileMiddleware = () => {
   const middleware: Middleware = async (ctx, next) => {
-    const path = await ctx.send({
-      root: `${Deno.cwd()}/public`,
-    });
-    if (path) {
-      return;
+    try {
+      const path = await ctx.send({
+        root: `${Deno.cwd()}/public`,
+      });
+      if (path) {
+        return;
+      }
+      await next();
+    } catch (_error: unknown) {
+      await next();
     }
-    await next();
   };
 
   return middleware;
