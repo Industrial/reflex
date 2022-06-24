8724e4286a61690d0a226d715f5bd797373d7dbc
Tom Wieland <tom.wieland@gmail.com>
2022-06-24 13:26:59 +0200

    refactor(deps): added esbuild to deps and started to group deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,12 @@
+// ESBuild
+export * as esbuild from 'https://deno.land/x/esbuild@v0.14.46/mod.js';
+export { denoPlugin } from 'https://deno.land/x/esbuild_deno_loader@0.5.0/mod.ts';
+
+// React
 export * as React from 'https://esm.sh/react@18.1.0';
+export { createElement } from 'https://esm.sh/react@18.1.0';
+export { renderToReadableStream } from 'https://esm.sh/react-dom@18.1.0/server';
+
 export type {
   ExportAllDeclaration,
   ExportNamedDeclaration,
@@ -11,7 +19,6 @@ export {
   Request as OakRequest,
 } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 export { Visitor } from 'https://esm.sh/@swc/core@1.2.171/Visitor.js';
-export { createElement } from 'https://esm.sh/react@18.1.0';
 export { createGraph } from 'https://deno.land/x/deno_graph@0.27.0/mod.ts';
 export { createHash } from 'https://deno.land/std@0.142.0/hash/mod.ts';
 export {
@@ -25,6 +32,6 @@ export {
   normalize,
   resolve,
 } from 'https://deno.land/std@0.140.0/path/mod.ts';
-export { renderToReadableStream } from 'https://esm.sh/react-dom@18.1.0/server';
 export { walk } from 'https://deno.land/std@0.140.0/fs/mod.ts';
 export { assertEquals } from 'https://deno.land/std@0.142.0/testing/asserts.ts';
+export { bundle, emit } from 'https://deno.land/x/emit@0.2.0/mod.ts';
